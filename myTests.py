c75fe6e4b9292ba14a09e619c7eb07c43acc60f2
eliyahu <eliyahu299@gmail.com>
2023-06-20 00:11:13 +0300

    removed my_tests

--- myTests.py
+++
@@ -1,7 +0,0 @@
-from ex11_utils import  find_length_n_paths,find_length_n_words
-
-# s=find_length_n_paths(3,[['A', 'B', 'C', 'D'], ['E', 'F', 'G', 'H'], ['I', 'G', 'K', 'L'], ['M', 'N', 'O', 'P']],('ABC', 'CDE', 'ABCD'))
-# print(s)
-
-s=find_length_n_words(3,[['A', 'B', 'C', 'D'], ['E', 'F', 'G', 'H'], ['I', 'G', 'K', 'L'], ['M', 'N', 'O', 'P']],('ABC', 'CDE', 'ABCD'))
-print(s)
