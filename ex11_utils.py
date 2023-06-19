d085301d9931390adb3c702af9d5e2687b496d45
eliyahu <eliyahu299@gmail.com>
2023-06-20 00:10:33 +0300

    finished the utils.

--- ex11_utils.py
+++ ex11_utils.py
@@ -1,33 +1,49 @@
 
 from typing import List, Tuple, Iterable, Optional
-from copy import copy,deepcopy
+from copy import copy, deepcopy
 import math
 
 Board = List[List[str]]
 Path = List[Tuple[int, int]]
 
-def distance(coor1,coor2):
+
+def distance(coor1, coor2):
     return math.sqrt((coor1[0]-coor2[0])**2 + (coor1[1]-coor2[1])**2)
 
-def is_valid_path(board: Board, path: Path, words: Iterable[str]) -> Optional[str]:
-    if not len(set(path)) == len(path): return None
-    str = ""
-    if not in_bound(path[0],board): return None
-    before = path[0]
-    str += board[before[0]][before[1]]
-    for i in range(1,len(path)):
-        coor = path[i]
-            #check if the coor is in bound
-        if not in_bound(coor,board): return None
-            #checking if the distance is bigger then 1
-        if not distance(before,coor) == 1: return None
-        str += board[coor[0]][coor[1]]
-        before = coor
-    if not str in words: return None
-    return str
 
-def in_bound(coordinate,board):
-    if not  0 <= coordinate[0] < len(board):
+def is_valid_path(board: Board, path: Path, words: Iterable[str]) -> Optional[str]:
+    if not path:
+        return None
+    
+    if len(path) == 0:
+        return None
+    
+    if not len(set(path)) == len(path):
+        return None
+    
+    st=""
+    
+    
+    for i,coor in  enumerate(path):
+        # check if the coor is in bound
+        if not in_bound(coor, board):
+            return None
+        
+        if i != 0:
+            before=path[i-1]            
+            ds=math.floor(distance(coor,before)) 
+            if ds != 1:
+                return None
+            
+        st += board[coor[0]][coor[1]]
+        
+    if not st in words:
+        return None
+    return st
+
+
+def in_bound(coordinate, board):
+    if not 0 <= coordinate[0] < len(board):
         return False
     if not 0 <= coordinate[1] < len(board[0]):
         return False
@@ -35,119 +51,110 @@ def in_bound(coordinate,board):
 
 
 def find_length_n_paths(n: int, board: Board, words: Iterable[str]) -> List[Path]:
-    valid_paths = []
-    def check_words(row: int, col: int, word: str, current_path: Path,visited:set):
-        # Base case: If the current path length exceeds n or the word does not match, stop the search
-        if len(current_path) > n or not word.startswith(board[row][col]):
-            return
-
-        # Update the word and current path
-        word = word[len(board[row][col]):]
-        current_path.append((row, col))
-
-        # Base case: If the current path length is n and the word is empty, add the current path to valid_paths
-        if len(current_path) == n and len(word) == 0:
-            valid_paths.append(list(current_path))
-
-        # Explore all possible neighbors
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                if i == 0 and j == 0:
-                    continue
-        
-                new_row, new_col = row + i, col + j
-
-                # Make sure the neighbor is within the board bounds
-                if 0 <= new_row < len(board) and 0 <= new_col < len(board[0]):
-                    if  (new_row, new_col) not in visited:
-                        
-                        # Add the neighbor to the visited set
-                        visited.add((new_row, new_col))
-
-                        # Recursive call to explore the neighbor
-                        check_words(new_row, new_col, word, current_path, visited)
-
-                        # Remove the neighbor from the visited set after the recursive call
-                        visited.remove((new_row, new_col))
+    results = []
+    words = list(sorted(words))
+    for i, row in enumerate(board):
+        for j, col in enumerate(row):
+            results.extend(find_length_n_paths_helper(
+                n, board, words, [(i, j)]))
+    return results
 
 
-        # Remove the last cell from the current path to backtrack
-        current_path.pop()
+def find_length_n_paths_helper(n: int, board: Board, words: list[str], path: list = []):
+    if not in_bound(path[-1], board):
+        return
 
-    # Iterate over each word and each cell on the board
-    for word in words:
-        for row in range(len(board)):
-            for col in range(len(board[0])):
-                # Perform check words starting from each cell
-                check_words(row, col, word, [],set())
+    if path[-1] in path[0:-2]:
+        return
 
-    return valid_paths
+    word = path_to_word(board, path)
+    words = list(filter(lambda x: x.startswith(word), words))
 
-      
+    if len(words) == 0:
+        return
+
+    if n == len(path):
+        if words[0] == word:
+            yield list(path)
+        return
+
+    row, col = path[-1]
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == 0 and j == 0:
+                continue
+            coordinate = (row+i, col+j)
+            path.append(coordinate)
+            yield from find_length_n_paths_helper(n, board, words, path)
+            path.pop()
+
+def path_to_word(board, path):
+    return "".join([board[i][j] for i, j in path])
+
+
 def find_length_n_words(n: int, board: Board, words: Iterable[str]) -> List[Path]:
-    result=[]
-    cleaned_words=filter(lambda x:len(x)==n,words)
-    for word in cleaned_words:
-       result.extend(find_length_n_word(n,board,word))
-    return result
+    results = []
+    words = filter(lambda x: len(x) == n, words)
+    words = sorted(words)
+    words = list(words)
+    for i, row in enumerate(board):
+        for j, col in enumerate(row):
+            results.extend(find_length_n_words_helper(
+                n, board, words, [(i, j)]))
+    return results
 
 
-def find_length_n_word(n:int,board:Board,word:str)->List[Path]:
+def find_length_n_words_helper(n: int, board: Board, words: list[str], path: list = []):
+    if not in_bound(path[-1], board):
+        return
+
+    if path[-1] in path[0:-2]:
+        return
+
+    word = "".join([board[i][j] for i, j in path])
+    words = list(filter(lambda x: x.startswith(word), words))
+
+    if len(words) == 0:
+        return
+
+    if n == len(word):
+        if words[0] == word:
+            yield list(path)
+        return
+
+    row, col = path[-1]
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == 0 and j == 0:
+                continue
+            coordinate = (row+i, col+j)
+            path.append(coordinate)
+            yield from find_length_n_words_helper(n, board, words, path)
+            path.pop()
+
+
+def max_score_paths(board,words):
+    existed=set()
     
-    def dfs(coor,word:str,visited:set=set()): 
-        nonlocal board        
-        if not in_bound(coor,board):
-            return []
-        
-        if coor in visited:
-            return []
-        
-        inner=board[coor[0]][coor[1]]
-
-        if not word.startswith(inner):
-            return []
-        
-        updated_word=word[len(inner):]
-
-        #base case
-        if updated_word == "":
-            return [[coor]]
-        
+    def filter_words(paths,board):
+        filtered=[]
+        for path in paths:
+            word = path_to_word(board,path)
+            if word in existed:
+                continue
+            existed.add(word)
+            filtered.append(path)
+        return filtered    
     
-        #dfs algorithim
-        visited.add(coor)
-        results=[]
-        
-        #will return [] in coor
-        for i in range(-1,2):
-             for j in range(-1,2):
-                new_coor = coor[0] + i, coor[1] + j
-                result_from=dfs(new_coor,updated_word,visited)
-                for result in result_from:
-                    result.extend(result_from)          
-        visited.remove(coor)
-        return results
-
-         
+    max_n = len(board) * len(board[0])
+    list_max_paths = []
+    for n in range(max_n,0,-1):
+        all_paths_len_n = find_length_n_paths(n,board,words)
+        list_max_paths.extend(filter_words(all_paths_len_n,board))
     
-    
-    result=[]
-    cols = len(board)
-    rows = len(board[0])
-    for col in range(cols):
-        for row in range(rows):
-            # current=board[col][row]
-           result.extend(dfs((col,row),word))
-    return result 
+    return list_max_paths
 
+   
+    
 
     
-        
-
-
-        
-
-
-# # def max_score_paths(board: Board, words: Iterable[str]) -> List[Path]:
-# #     pass
-
