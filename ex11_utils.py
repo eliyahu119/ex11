494805ae1b3f335fc287557b8768a82043fe4f19
Eliyahu levy <Eliyahu299@gmail.com>
2023-06-19 16:36:08 +0300

    to be continue

--- ex11_utils.py
+++ ex11_utils.py
@@ -1,6 +1,6 @@
 
 from typing import List, Tuple, Iterable, Optional
-from copy import copy
+from copy import copy,deepcopy
 
 Board = List[List[str]]
 Path = List[Tuple[int, int]]
@@ -9,45 +9,64 @@ Path = List[Tuple[int, int]]
 def is_valid_path(board: Board, path: Path, words: Iterable[str]) -> Optional[str]:
     pass
 
-def if_bound(coordinate,board):
+def in_bound(coordinate,board):
     return 0 <= coordinate[0] < len(board) and 0 <= coordinate[1] < len(board[0])
 
 def find_length_n_paths(n: int, board: Board, words: Iterable[str]) -> List[Path]:
-    results=[]
-    memo={}
+        results=[]
+        memo={}
+        hashed_words=set(words)
+        
+        def all_possible_words(coor,n,has_been:set=None)->List[Tuple[str,Path]]:
+                if has_been == None:
+                    has_been=set()
 
+                nonlocal board
+                if n <= 0:
+                    return []
+                if not in_bound(coor,board):
+                    return [] 
+                if coor in  has_been:
+                    return []
+                has_been.add(coor)
+
+                if n == 1:
+                    string=board[coor[0]][coor[1]]
+                    result=[(string,[coor])]
+                    return result
+                
+                if (coor,n) in memo:
+                    return  deepcopy(memo[(coor,n)])
+                
+                results=[]
+                for dir in get_possible_directions(coor):
+                    n_1_results=deepcopy(all_possible_words(dir,n-1,has_been))
+                    for result in n_1_results:
+                        st,path=result
+                        path=[coor,*path]
+                        st=board[coor[0]][coor[1]]+st
+                        results.append((st,path))
+                memo[(coor,n)]=results
+                return results
+  
+
+
+        possible_results=[]
+        height = len(board)
+        width = len(board[0]) if board[0] else 0
     
-    def find_words(coor,n,path:list=[],word="",has_been:set=set())->List[Path]:
-        nonlocal words
-        nonlocal board
-        nonlocal results
+        for col in range(height):
+            for row in range(width):
+                result=all_possible_words((col,row),n)
+                possible_results.extend(result)
+    
+        results = []
+        for (word,path) in possible_results:
+            if word in hashed_words:
+                results.append(path)
 
-        if n <= 0:
-            if word in words:
-                results.append(copy(path))
-            return 
-        
-        if not if_bound(coor,board):
-            return 
-        
-        if coor  in has_been:
-            return 
-        
-        has_been.add(coor)
-        path.append(coor)
-        word+=board[coor[0]][coor[1]]
-        all_possible_dirs=get_possible_directions(coor)
-        for dir in all_possible_dirs:
-             find_words(dir,n-1,path,word,has_been)
+        return results
 
-
-    height = len(board)
-    width = len(board[0]) if board[0] else 0
-    for col in range(height):
-        for row in range(width):
-            find_words((col,row),n-1)
-
-    return results
 
 
     
@@ -90,5 +109,5 @@ def get_possible_directions(coor):
 
 
 
-def max_score_paths(board: Board, words: Iterable[str]) -> List[Path]:
-    pass
+# def max_score_paths(board: Board, words: Iterable[str]) -> List[Path]:
+#     pass
