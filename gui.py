7fc79af5131fe0bb0a2cfaf6f2e17570d2e04be3
alon1325 <alenenok9@gmail.com>
2023-06-24 23:12:22 +0300

    23.12

--- gui.py
+++ gui.py
@@ -2,6 +2,9 @@ import string
 from typing import Dict, List, Callable, Any
 import tkinter as tk
 import time
+
+from boggle import Boggle
+
 ROOT_NAME = "Boggle"
 BUTTON_HOVER_COLOR = 'gray'
 REGULAR_COLOR = 'lightgray'
@@ -15,12 +18,11 @@ BUTTON_STYLE = {"font": ("Courier", 30),
 ROWS =4
 COLS=4
 class Game:
-    buttons: Dict[str, tk.Button] = {}
-    def __init__(self,table):   #basic init
-        self.duration =180
-        self.root = tk.Tk()
-        self.root.title(ROOT_NAME)
-        self.table=table
+    def __init__(self,boggle:Boggle):   #basic init
+        self.__duration =180
+        self.__root = tk.Tk()
+        self.__root.title(ROOT_NAME)
+        self.__boggle=boggle
 
         self.create_gui()
 #---------------------------------create all formats
@@ -28,12 +30,12 @@ class Game:
     def create_gui(self):
         self.create_upper_frame()
         self.create_display_label()
-        self.create_buttons_grid(self.table)
+        self.create_buttons_grid(self.__boggle)
         self.create_buttom_format()
 
     # Create a timer and score frame
     def create_upper_frame(self):
-        self.upper_frame = tk.Frame(self.root)
+        self.upper_frame = tk.Frame(self.__root)
         self.upper_frame.pack(pady=10, fill=tk.BOTH)
 
         self.timer_label = tk.Label(self.upper_frame, text="", font=("Helvetica", 24, "bold"), justify='l')
@@ -44,21 +46,21 @@ class Game:
 
     # create a label for word
     def create_display_label(self):
-        self._display_label = tk.Label(self.root, font=("Courier", 30),
+        self._display_label = tk.Label(self.__root, font=("Courier", 30),
                                        bg="lightgray", width=23, relief="ridge")
         self._display_label.pack(side=tk.TOP, fill=tk.BOTH)
 
     # create a frame for buttons
     def create_buttons_grid(self,table):
-        self.button_grid_frame = tk.Frame(self.root)
+        self.button_grid_frame = tk.Frame(self.__root)
         self.button_grid_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        self.create_buttons_in_lower_frame(table)
+        self.create_buttons_in_lower_frame()
 
 
     # create buttom format
     def create_buttom_format(self):
-        self.buttom_frame = tk.Frame(self.root)
+        self.buttom_frame = tk.Frame(self.__root)
         self.buttom_frame.pack(fill=tk.BOTH)
 
         self.save_word_button = tk.Button(self.buttom_frame,**BUTTON_STYLE,text= "Save\nword")
@@ -78,17 +80,19 @@ class Game:
             self.timer_label.after(1000, self.update_timer)
         else:
             self.timer_label.config(text="Time's up!")
+    def update_score(self):
+        self.score_label.config(text= "score: {:02d}".format(self.__boggle.get_score()))
     #run methos(incomplete)
     def run(self):
-        self.end_time = time.time() + self.duration
+        self.end_time = time.time() + self.__duration
         self.update_timer()
-        self.root.mainloop()
-        self.root.mainloop()
+        self.__root.mainloop()
+        self.__root.mainloop()
 
 #---------------------------------buttons grid and button settings
 
 
-    def create_buttons_in_lower_frame(self,table) -> None:
+    def create_buttons_in_lower_frame(self) -> None:
         #create grid table
         for i in range(COLS):
             tk.Grid.columnconfigure(self.button_grid_frame, i, weight=1)  # type: ignore
@@ -103,7 +107,7 @@ class Game:
             row_buttons = []
             for col in range(COLS):
                 #create button and place in grid
-                button = tk.Button(self.button_grid_frame, text=table[row][col].upper(), **BUTTON_STYLE,command= lambda r=row,c=col: self._display_label.config(text=self.table[r][c]))
+                button = tk.Button(self.button_grid_frame, text=self.__boggle.get_value_by_location(row,col).upper(), **BUTTON_STYLE,command= lambda r=row,c=col: self._display_label.config(text=self.__boggle.get_value_by_location(r,c)))
                 button.grid(row=row, column=col, sticky=tk.NSEW)
                 #make buttons shine if mouse on him
                 self.change_button_color_on_hover(button,BUTTON_HOVER_COLOR,REGULAR_COLOR)
@@ -124,11 +128,6 @@ class Game:
         button.bind("<Leave>", on_leave)
 #--------------------------------------------------------
 
-a = Game([
-    [ "Ce", "D", "E", "F"],
-    ["G", "H", "I", "J", "K", "L"],
-    ["M", "N", "O", "P", "Q", "R"],
-    ["C", "D", "E", "F"]
-
-])
+b= Boggle()
+a=Game(b)
 a.run()
