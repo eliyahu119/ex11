5066f5ed15de5c225799e16461e025f8e3f8dc98
alon1325 <alenenok9@gmail.com>
2023-06-25 01:52:44 +0300

    1.52

--- gui.py
+++ gui.py
@@ -22,130 +22,134 @@ class Game:
         self.__boggle=boggle
         self.__boggle.start_game()
         self.__current_word = ""
-        self.create_gui()
+        self.__create_gui()
 
 #---------------------------------create all formats
     #create all
-    def create_gui(self):
+    def __create_gui(self):
         self.__root = tk.Tk()
         self.__root.title(ROOT_NAME)
-        self.create_upper_frame()
-        self.create_display_label()
-        self.create_buttons_grid(self.__boggle)
-        self.create_buttom_format()
+        self.__create_upper_frame()
+        self.__create_display_label()
+        self.__create_buttons_grid(self.__boggle)
+        self.__create_buttom_format()
 
     # Create a timer and score frame
-    def create_upper_frame(self):
-        self.upper_frame = tk.Frame(self.__root)
-        self.upper_frame.pack(pady=10, fill=tk.BOTH)
+    def __create_upper_frame(self):
+        self.__upper_frame = tk.Frame(self.__root)
+        self.__upper_frame.pack(pady=10, fill=tk.BOTH)
+        self.__create_timer_label()
+        self.__create_score_label()
+    def __create_timer_label(self):
+        self.__timer_label = tk.Label(self.__upper_frame, text="", font=("Helvetica", 24, "bold"), justify='l')
+        self.__timer_label.pack(side=tk.LEFT)
+    def __create_score_label(self):
+        self.__score_label = tk.Label(self.__upper_frame, text="score: 0", font=("Helvetica", 24, "bold"), justify='r')
+        self.__score_label.pack(side=tk.RIGHT)
 
-        self.timer_label = tk.Label(self.upper_frame, text="", font=("Helvetica", 24, "bold"), justify='l')
-        self.timer_label.pack(side=tk.LEFT)
+    def __create_display_label(self): # create a label for current word
+        self.__display_label = tk.Label(self.__root, font=("Courier", 30),
+                                        bg="lightgray", width=23, relief="ridge")
+        self.__display_label.pack(side=tk.TOP, fill=tk.BOTH)
 
-        self.score_label = tk.Label(self.upper_frame, text="score: 0", font=("Helvetica", 24, "bold"), justify='r')
-        self.score_label.pack(side=tk.RIGHT)
 
-    # create a label for word
-    def create_display_label(self):
-        self._display_label = tk.Label(self.__root, font=("Courier", 30),
-                                       bg="lightgray", width=23, relief="ridge")
-        self._display_label.pack(side=tk.TOP, fill=tk.BOTH)
-
-    # create a frame for buttons
-    def create_buttons_grid(self,table):
-        self.button_grid_frame = tk.Frame(self.__root)
-        self.button_grid_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
-        self.create_buttons_in_lower_frame()
+    def __create_buttons_grid(self, table): # create a frame for buttons
+        self.__button_grid_frame = tk.Frame(self.__root)
+        self.__button_grid_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        self.__create_buttons_in_lower_frame()
 
 
     # create buttom format
-    def create_buttom_format(self):
-        self.buttom_frame = tk.Frame(self.__root)
-        self.buttom_frame.pack(fill=tk.BOTH)
+    def __create_buttom_format(self):
+        self.__buttom_frame = tk.Frame(self.__root)
+        self.__buttom_frame.pack(fill=tk.BOTH)
+        self.__create_save_word_btn()
+        self.__create_used_words_label()
+    def __create_save_word_btn(self):
+        self.__save_word_button = tk.Button(self.__buttom_frame, **BUTTON_STYLE,
+                                            text="Save\nword", command=self.__save_word)
+        self.__save_word_button.pack(side=tk.LEFT)
+    def __create_used_words_label(self):
+        self.__used_words = tk.Label(self.__buttom_frame, justify='l', text ="", font=("Courier", 8),
+                                     bg="lightgray", width=23, relief="ridge")
+        self.__used_words.pack(side=tk.LEFT, fill=tk.BOTH, expand='true')
 
-        self.save_word_button = tk.Button(self.buttom_frame,**BUTTON_STYLE,text= "Save\nword",command= self.save_word)
-        self.save_word_button.pack(side=tk.LEFT)
-
-        self.used_words = tk.Label(self.buttom_frame,justify='l',text = "",font=("Courier", 8),
-                                       bg="lightgray", width=23, relief="ridge")
-        self.used_words.pack(side=tk.LEFT,fill=tk.BOTH,expand='true')
-
-    def save_word(self):
+    def __save_word(self):
         self.__boggle.submit_word()
         self.__current_word = ""
-        self.update_current_word()
-        self.update_score()
-        self.update_used_words_screen()
-#-------------------------------------------------#
-    #timer
-    def update_timer(self):
+        self.__update_current_word()
+        self.__update_score()
+        self.__update_used_words_screen()
+#------------------------------------------------- screen updates
+
+    def __update_timer(self):
         remaining_time = self.__boggle.get_current_time()
         if remaining_time > 0:
             minutes = int(remaining_time // 60)
             seconds = int(remaining_time % 60)
-            self.timer_label.config(text="time: {:02d}:{:02d}".format(minutes, seconds))
-            self.timer_label.after(1000, self.update_timer)
+            self.__timer_label.config(text="time: {:02d}:{:02d}".format(minutes, seconds))
+            self.__timer_label.after(1000, self.__update_timer)
         else:
             self.__root.destroy()
-    def update_score(self):
-        self.score_label.config(text= "score:{:2d}".format(self.__boggle.score))
+    def __update_score(self):
+        self.__score_label.config(text="score:{:2d}".format(self.__boggle.score))
 
-    def update_used_words_screen(self):
+    def __update_used_words_screen(self):
         str = ""
         lst = self.__boggle.used_words
         for word in lst:
             str += word
             str += " "
-        self.used_words.config(text=str)
+        self.__used_words.config(text=str)
+
+    def __update_current_word(self):
+        self.__display_label.config(text=self.__current_word)
 
     #run methos(incomplete)
     def run(self):
-        self.update_timer()
+        "run the game"
+        self.__update_timer()
         self.__root.mainloop()
 
 #---------------------------------buttons grid and button settings
-
-
-    def create_buttons_in_lower_frame(self) -> None:
-        #create grid table
+    def __create_buttons_in_lower_frame(self) -> None:
+        "summerize the grid button building"
+        self.__create_grid_configure()
+        self.__create_buttons_on_grid()
+    def __create_grid_configure(self):
+        "create the figure of the grid table"
         for i in range(COLS):
-            tk.Grid.columnconfigure(self.button_grid_frame, i, weight=1)  # type: ignore
-
+            tk.Grid.columnconfigure(self.__button_grid_frame, i, weight=1)  # type: ignore
         for i in range(ROWS):
-            tk.Grid.rowconfigure(self.button_grid_frame, i, weight=1)  # type: ignore
-        self.grid_buttons = []
+            tk.Grid.rowconfigure(self.__button_grid_frame, i, weight=1)  # type: ignore
 
+    def __create_buttons_on_grid(self):
+        "create the buttons on the grid"
         for row in range(ROWS):
-            row_buttons = []
             for col in range(COLS):
                 #create button and place in grid
-                button = tk.Button(self.button_grid_frame, text=self.__boggle.get_value_by_location(row,col).upper(),
-                                   **BUTTON_STYLE, command= lambda r=row,c=col: self.grid_button_click(r,c))
+                button = tk.Button(self.__button_grid_frame, text=self.__boggle.get_value_by_location(row, col).upper(),
+                                   **BUTTON_STYLE, command= lambda r=row,c=col: self.__grid_button_click(r, c))
                 button.grid(row=row, column=col, sticky=tk.NSEW)
                 #make buttons shine if mouse on him
-                self.change_button_color_on_hover(button,BUTTON_HOVER_COLOR,REGULAR_COLOR)
-                #append button
-                row_buttons.append(button)
-            self.grid_buttons.append(row_buttons)
+                self.__change_button_color_on_hover(button, BUTTON_HOVER_COLOR, REGULAR_COLOR)
 
-    def grid_button_click(self,x,y):
+    def __grid_button_click(self, x, y):
+        "grid button onclick, add the char to current word"
         self.__boggle.add_to_current_path(x,y)
         self.__current_word += self.__boggle.get_value_by_location(x,y)
-        self.update_current_word()
+        self.__update_current_word()
 
-    def update_current_word(self):
-        self._display_label.config(text=self.__current_word)
-    #make button shine when mouse on him
-    def change_button_color_on_hover(self,button, hover_color, regular_color):
-        def on_enter(event):
+    def __change_button_color_on_hover(self, button, hover_color, regular_color):
+        "make button shine when mouse on him"
+        def __on_enter(event):
             button.config(background=hover_color)
 
-        def on_leave(event):
+        def __on_leave(event):
             button.config(background=regular_color)
 
-        button.bind("<Enter>", on_enter)
-        button.bind("<Leave>", on_leave)
+        button.bind("<Enter>", __on_enter)
+        button.bind("<Leave>", __on_leave)
 #--------------------------------------------------------
 
 if __name__ == '__main__':
