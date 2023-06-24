10a4be88461814caf23fde8e7da9808c054a3b4c
alon1325 <alenenok9@gmail.com>
2023-06-24 21:25:28 +0300

    21.25 boggle

--- boggle.py
+++ boggle.py
@@ -1,24 +1,26 @@
 from count_down_timer import CountdownTimer
-
+import sys,boggle_board_randomizer
+import ex11_utils
 GAME_DURATION=3*60
 
-
-class Boggel:
+class Boggle:
 
     def __init__(self) -> None:
         self.__score=0
-        self.__current_path=set()
+        self.__current_path=[]
+        self.__current_word = ""
         self.__timer=CountdownTimer(GAME_DURATION)
-    
+        self.__board = self.__init_game_board()
+        self.__words_set = self.init_words_set()
+        self.__max_score = self.get_max_possible_score()
     @property 
     def score(self):
         return self.__score
     
-    def __init_game_board(self):
-        #create a matrix, for the game board
-        pass
+    def init_game_board(self):
+        return boggle_board_randomizer.randomize_board()
 
-    def init_word_dict(self):
+    def init_words_set(self): #load words set
         try:
             words_set = {}
             file = open(sys.argv[1], "r")
@@ -33,30 +35,34 @@ class Boggel:
     def get_value_by_location(self,x,y):
         #returns the location value base
         # return matrix[x][y]
-        pass
+        return self.__board[x][y]
+
 
     def add_to_current_path(self,x,y):
         #check if its alright,
         #add to path
-        pass
+        if not self.is_location_disabled(x,y):
+            self.__current_path.append((x,y))
+            self.__current_word += self.__board[x][y]
 
     def is_location_disabled(self,x,y):
         #check if the button is disabled.
-        return (x,y) in self.path
+        return (x,y) in self.__current_path
 
-    def game_over(self):
+    def is_game_over(self):
         #check if all the conditions are met for the game to be over
-        pass
+        return self.__timer.get_remaining_time() == 0 or self.__score == self.__max_score
 
     def get_current_time(self)->str:
         # return timer
         return self.__timer.get_remaining_time()
         
     def current_word(self)->str:
-        pass
-    
+        return self.__current_word
+
     def current_score(self)->str:
-        pass
+        return self.__score
+
 
     def start_game(self)->None:
         #start the game
@@ -65,6 +71,13 @@ class Boggel:
         #init score
         #init i
         pass
-    
-    
+
+    def get_max_possible_score(self):
+        paths_list = ex11_utils.max_score_paths(self.__board,self.__words_set)
+        max_score = 0
+        for path in paths_list:
+            max_score += len(path)**2
+        return max_score
+
+
     
