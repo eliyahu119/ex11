78164ba9018fc748755c2139bc99b26385e3cf6b
eliyahu <eliyahu299@gmail.com>
2023-06-27 02:20:56 +0300

    added deploy

--- deploy.py
+++ deploy.py
@@ -9,10 +9,11 @@ def create_zip(files, folder_path, zip_name):
                 zipf.write(file_path, arcname=file)
 
 # Example usage
-files =['boggle_logic.py', 'boggle.py', 'snake.py', 'snake_game.py', 'snake_main.py', 'snake_types.py',
-  'utils.py', 'wall.py',"AUTHORS"]
+files =['boggle_logic.py', 'boggle.py', 'count_down_timer.py', 'end_screen.py',
+        'ex11_utils.py', 'gui.py.py',"open_screen.py ","path.py"
+  'boggle_dict.txt', 'boggle_board_randomizer.py',"AUTHORS"]
 # Example usage
 current_folder = os.getcwd()  # Get the current working directory
-zip_name = os.path.join(current_folder, 'ex10.zip')
+zip_name = os.path.join(current_folder, 'ex11.zip')
 
 create_zip(files, current_folder, zip_name)
