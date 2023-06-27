10f2d2b452e4b2974e9c6caf3aa4120845ed81c9
alon1325 <alenenok9@gmail.com>
2023-06-27 13:53:40 +0300

    13.53

--- path.py
+++ path.py
@@ -1,16 +1,21 @@
 
 class Path:
+    """The class describe path as list of coordinates"""
     def __init__(self):
+        """Create new path"""
         self.__path = []
 
     def add_to_path(self,coor):
+        """Add new coordinate to path"""
         self.__path.append(coor)
 
 
     def get_path(self):
+        """Get the path as list"""
         return list(self.__path)
     
     def clear(self):
+        """Remove all coordinates of path"""
         self.__path = []
 
 
